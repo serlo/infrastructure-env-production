e382cd857cdd11bbc74cbff4f6ba79b114e5d11c
Jonas Keinholz <git@inyono.dev>
2020-11-16 03:17:16 +0100

    move provider versions into required_providers

--- providers.tf
+++ providers.tf
@@ -1,10 +1,8 @@
 provider "cloudflare" {
-  version   = "2.13.2"
   api_token = var.cloudflare_token
 }
 
 provider "google" {
-  version     = "3.47.0"
   project     = local.project
   credentials = file(local.credentials_path)
 }
@@ -16,7 +14,6 @@ provider "google-beta" {
 }
 
 provider "helm" {
-  version = "1.3.2"
   kubernetes {
     host     = module.cluster.endpoint
     username = ""
@@ -29,27 +26,10 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  version          = "1.13.2"
   host             = module.cluster.endpoint
   load_config_file = false
 
   client_certificate     = base64decode(module.cluster.auth.client_certificate)
   client_key             = base64decode(module.cluster.auth.client_key)
   cluster_ca_certificate = base64decode(module.cluster.auth.cluster_ca_certificate)
-}
-
-provider "null" {
-  version = "2.1.2"
-}
-
-provider "random" {
-  version = "2.2.1"
-}
-
-provider "template" {
-  version = "2.1.2"
-}
-
-provider "tls" {
-  version = "2.1.1"
 }
