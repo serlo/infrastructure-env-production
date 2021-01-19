2d3884c3a95fbe323e4b3ba9cddb8b8430f0cc33
Jonas Keinholz <git@inyono.dev>
2021-01-19 04:45:01 +0100

    rocket chat maintenance

--- rocket-chat.tf
+++ rocket-chat.tf
@@ -1,19 +1,24 @@
 locals {
   rocket_chat = {
-    chart_version = "2.0.10"
-    image_tag     = "3.8.1"
+    chart_versions = {
+      rocketchat = "2.0.10"
+      mongodb    = "10.4.0"
+    }
+    image_tags = {
+      rocketchat = "3.10.4"
+      mongodb    = "4.2.11"
+    }
   }
 }
 
 module "rocket-chat" {
-  //  source = "github.com/serlo/infrastructure-modules-shared.git//rocket-chat?ref=v2.0.0"
-  source = "../modules-shared/rocket-chat"
+  source = "github.com/serlo/infrastructure-modules-shared.git//rocket-chat?ref=v3.0.0"
 
-  host          = "community.${local.domain}"
-  namespace     = kubernetes_namespace.community_namespace.metadata.0.name
-  chart_version = local.rocket_chat.chart_version
-  image_tag     = local.rocket_chat.image_tag
-  app_replicas  = 2
+  host           = "community.${local.domain}"
+  namespace      = kubernetes_namespace.community_namespace.metadata.0.name
+  chart_versions = local.rocket_chat.chart_versions
+  image_tags     = local.rocket_chat.image_tags
+  app_replicas   = 1
 
   mongodump = {
     image         = "eu.gcr.io/serlo-shared/mongodb-tools-base:1.0.1"
