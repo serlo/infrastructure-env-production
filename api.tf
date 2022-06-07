b16c6bafcd4789a04a70bc666d16b3aa9bcb58fb
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-06-07 21:52:54 +0200

    Upgrade api module

--- api.tf
+++ api.tf
@@ -1,8 +1,8 @@
 locals {
   api = {
     image_tags = {
-      database_layer = "0.3.42"
-      server         = "0.33.1"
+      database_layer = "0.3.47"
+      server         = "0.36.0"
       cache_worker   = "0.4.2"
     }
   }
