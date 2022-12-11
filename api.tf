7edca1242cd7fc3a335203d0d8acde62b0d0914c
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-12-11 23:48:36 +0100

    Maintain current behavior of discussions

--- api.tf
+++ api.tf
@@ -1,8 +1,8 @@
 locals {
   api = {
     image_tags = {
-      database_layer = "0.3.57"
-      server         = "0.47.1"
+      database_layer = "0.3.55"
+      server         = "0.47.0"
       cache_worker   = "0.4.2"
     }
   }
