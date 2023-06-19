d6516be426ef74055fac3c9dc0925a9f0abb4d79
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-06-19 11:06:04 +0200

    Upgrade api and db layer images

--- api.tf
+++ api.tf
@@ -1,8 +1,8 @@
 locals {
   api = {
     image_tags = {
-      database_layer   = "0.3.63"
-      server           = "0.50.1"
+      database_layer   = "0.3.65"
+      server           = "0.51.1"
       cache_worker     = "0.4.2"
       api_db_migration = "0.1.0"
     }
