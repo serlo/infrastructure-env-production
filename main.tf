1fda4f2778ff4f90d83c365e16f5beb7ca978127
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-11-27 21:43:20 +0100

    Upgrade cf module

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ module "ingress-nginx" {
 }
 
 module "cloudflare" {
-  source  = "github.com/serlo/infrastructure-modules-env-shared.git//cloudflare?ref=v6.1.1"
+  source  = "github.com/serlo/infrastructure-modules-env-shared.git//cloudflare?ref=v7.1.0"
   domain  = local.domain
   ip      = module.cluster.address
   zone_id = "1a4afa776acb2e40c3c8a135248328ae"
